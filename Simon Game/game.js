27e6d7a238b95cae87540e0314e6cf6f6c8ab204
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2021-12-10 14:01:59 +0530

    Create game.js

---
+++ Simon Game/game.js
@@ -0,0 +1,77 @@
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+let level = 0;
+
+
+$("body").on("keypress", function() {
+  if (level ===0) {
+    nextSequence();
+  }
+})
+
+$(".btn").on("click", function() {
+  let userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
+})
+
+function nextSequence() {
+  level++;
+  $("h1").text("level "+level);
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel]===gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+        userClickedPattern = [];
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+  }, 200);
+  $("h1").text("Game Over, Press Any Key to Restart");
+  startOver();  
+}
+
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#"+currentColor).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColor).removeClass("pressed");
+}, 100);
+}
+
+
+
+
